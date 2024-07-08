a47ceca86b45984440589b7287e21cc21c48d209
daorozc <daorozc@bancolombia.com.co>
2024-07-08 17:53:10 -0500

    Added the ability to export Kiuwan application information to JSON files.

--- kiuwan_info.py
+++ kiuwan_info.py
@@ -1,5 +1,6 @@
+import json
 from kiuwan_endpoints import get_kiuwan_applications, get_application_info
-from utils import filter_as400_applications
+from utils import filter_as400_applications, export_app_info_to_json
 
 if __name__ == "__main__":
     print("\nFetching Kiuwan applications:")
@@ -7,9 +8,10 @@ if __name__ == "__main__":
     iseries_applications = filter_as400_applications(all_applications)
     print(iseries_applications)
 
-    print("\nFetching info and defects for iSeries applications:")
+    print("\nFetching info for iSeries applications and exporting to JSON:")
     for app in iseries_applications:
         app_name = app['name']
         print(f"\nInfo for {app_name}:")
         app_info = get_application_info(app_name)
         print(app_info)
+        export_app_info_to_json(app_name, app_info)
--- utils.py
+++ utils.py
@@ -1,5 +1,16 @@
+import json
+
 def filter_as400_applications(applications):
     """
     Filter applications to include only those with quality_model "AS400"
     """
     return [app for app in applications if app.get('quality_model') == 'AS400']
+
+def export_app_info_to_json(app_name, app_info):
+    """
+    Export application info to a JSON file.
+    """
+    filename = f"{app_name.replace(' ', '_')}_info.json"
+    with open(filename, 'w') as f:
+        json.dump(app_info, f, indent=4)
+    print(f"Exported {filename}")
