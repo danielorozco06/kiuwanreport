69b5ccbe34cbbf3b5146b472b960db106cbbb243
daorozc <daorozc@bancolombia.com.co>
2024-07-08 17:23:10 -0500

    Removed the `get_kiuwan_info()` function and its usage, focusing the script on fetching and filtering Kiuwan applications.

--- kiuwan_info.py
+++ kiuwan_info.py
@@ -2,10 +2,6 @@ from kiuwan_endpoints import get_kiuwan_info, get_kiuwan_applications
 from utils import filter_as400_applications
 
 if __name__ == "__main__":
-    print("Fetching Kiuwan info:")
-    info = get_kiuwan_info()
-    print(info)
-
     print("\nFetching Kiuwan applications:")
     all_applications = get_kiuwan_applications()
     iseries_applications = filter_as400_applications(all_applications)
