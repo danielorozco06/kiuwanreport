ef403e8e1c3ad6cbad115dea4c4af721db43a697
daorozc <daorozc@bancolombia.com.co>
2024-07-10 15:58:23 -0500

    Refactored the kiuwan_info.py script to fetch and export application information for all Kiuwan applications, with the iSeries applications having their names formatted appropriately.

--- kiuwan_info.py
+++ kiuwan_info.py
@@ -1,16 +1,12 @@
-from kiuwan_endpoints import get_kiuwan_applications, get_application_info
-from utils import filter_as400_applications, export_app_info_to_json
+from kiuwan_endpoints import get_application_info, get_kiuwan_applications
+from utils import export_app_info_to_json
 
 if __name__ == "__main__":
     print("\nFetching Kiuwan applications:")
     all_applications = get_kiuwan_applications()
-    iseries_applications = filter_as400_applications(all_applications)
-    print(iseries_applications)
 
     print("\nFetching info for iSeries applications and exporting to JSON:")
-    for app in iseries_applications:
-        app_name = app['name']
-        print(f"\nInfo for {app_name}:")
-        app_info = get_application_info(app_name)
-        print(app_info)
-        export_app_info_to_json(app_name, app_info)
+    for app in all_applications:
+        app_info = get_application_info(app["name"])
+        print(f"\nInfo for {app['name']}:")
+        export_app_info_to_json(app_info)
--- utils.py
+++ utils.py
@@ -1,19 +1,25 @@
 import json
 import os
 
-def filter_as400_applications(applications):
-    """
-    Filter applications to include only those with quality_model "AS400"
-    """
-    return [app for app in applications if app.get('quality_model') == 'AS400']
 
-def export_app_info_to_json(app_name, app_info):
+def validate_and_format_app_name(app_info):
+    """
+    Validate the quality_model and format the app name if necessary.
+    """
+    if app_info.get("quality_model") == "AS400":
+        app_info["name"] = f"IS{app_info['name']}0001"
+    return app_info
+
+
+def export_app_info_to_json(app_info):
     """
     Export application info to a JSON file in the output/ directory.
     """
-    output_dir = 'output'
+    app_info = validate_and_format_app_name(app_info)
+    output_dir = "output"
     os.makedirs(output_dir, exist_ok=True)
-    filename = os.path.join(output_dir, f"{app_name.replace(' ', '_')}_info.json")
-    with open(filename, 'w') as f:
+
+    filename = os.path.join(output_dir, f"{app_info['name']}.json")
+    with open(filename, "w") as f:
         json.dump(app_info, f, indent=4)
     print(f"Exported {filename}")
